0cd26455b4bda50184f08bc9b577b44cc34dc5c4
Sylvain BIGONNEAU <sbigonneau@sedona.fr>
2017-01-24 17:19:32 +0100

    ENH : authorizations

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -2,6 +2,10 @@ defmodule Discuss.TopicController do
     use Discuss.Web, :controller
 
     alias Discuss.Topic
+    alias Discuss.User
+
+    plug Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+    plug :check_topic_owner when action in [:edit, :update, :delete]
 
     def index(conn, _params) do
         topics = Repo.all(Topic)
@@ -16,7 +20,9 @@ defmodule Discuss.TopicController do
     end
 
     def create(conn, %{"topic" => topic}) do
-        changeset = Topic.changeset(%Topic{}, topic)
+        changeset = conn.assigns.user
+        |> build_assoc(:topics)
+        |> Topic.changeset(topic)
 
         case Repo.insert(changeset) do
             {:ok, _post} ->
@@ -61,4 +67,16 @@ defmodule Discuss.TopicController do
         |> redirect(to: topic_path(conn, :index))
     end
 
+    def check_topic_owner(conn, _params) do
+        %{params: %{"id" => topic_id}} = conn
+
+        if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
+            conn
+        else
+            conn
+            |> put_flash(:error, "You cannot edit that")
+            |> redirect(to: topic_path(conn, :index))
+            |> halt()
+        end
+    end
 end
